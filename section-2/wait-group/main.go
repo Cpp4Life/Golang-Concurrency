f1a79992def914e33f7a2da40d0063dab807aca6
Cpp4Life <Cpp4Life@users.github.com>
2022-08-14 22:04:51 +0700

    first commit

---
+++ section-2/wait-group/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(s)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	words := []string{"alpha", "beta", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliett", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu"}
+
+	wg.Add(len(words))
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
+
+	wg.Add(1)
+	printSomething("Done", &wg)
+}
---
+++ section-3/race-condition-mutex/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+type Income struct {
+	Source string
+	Amount int
+}
+
+func main() {
+	// variables for bank balance
+	var bankBalance int
+	var balance sync.Mutex
+
+	// print out starting values
+	fmt.Printf("Initial bank balance: $%d.00\n", bankBalance)
+
+	// define weekly revenue
+	incomes := []Income{
+		{"Main job", 500},
+		{"Gifts", 10},
+		{"Part-time job", 50},
+		{"Investments", 100},
+	}
+
+	wg.Add(len(incomes))
+
+	// loop through 52 weeks and print out how much is made; keep a running total
+	for i, income := range incomes {
+		go func(i int, income Income) {
+			defer wg.Done()
+
+			for week := 1; week <= 52; week++ {
+				balance.Lock()
+				temp := bankBalance
+				temp += income.Amount
+				bankBalance = temp
+				balance.Unlock()
+
+				fmt.Printf("On week %d, you earned $%d.00 from %s\n", week, income.Amount, income.Source)
+			}
+		}(i, income)
+	}
+
+	wg.Wait()
+
+	// print out final balance
+	fmt.Printf("Final bank balance: $%d.00\n", bankBalance)
+}
